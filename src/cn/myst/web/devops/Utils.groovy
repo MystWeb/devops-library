fa65fd11dbec4e05dc499d42cc304d6305f0538a
Myst <40738093+MystWeb@users.noreply.github.com>
2022-07-12 16:14:12 +0800

    Update Utils.groovy

--- src/cn/myst/web/devops/Utils.groovy
+++ src/cn/myst/web/devops/Utils.groovy
@@ -4,9 +4,7 @@ package cn.myst.web.devops
  * @author ziming.xing
  * Create Date：2022/7/12
  */
-class Utils {
-    // 消息打印函数
-    def PrintMsg(value) {
-        println(value)
-    }
+// 消息打印函数
+def PrintMsg(value) {
+    println(value)
 }
