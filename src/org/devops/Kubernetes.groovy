5aeee034bc8d43201037b9c3e74d4988e324b0c3
ziming.xing <ziming.xing@proaimltd.com>
2022-11-02 16:27:31 +0800

    feat(新增：GitOps-基于Helm的CI/CD（发布模板文件下载与模板文件内容替换后上传远程GitLab代码仓库、helm部署应用、应用健康检查、应用版本回滚）):
    HelmReleaseTemplateFileReplaceAndConvertToBase64：Kubernetes-Helm发布模板文件内容替换并转换Base64后上传远程GitLab代码仓库（CI）
    HelmReleaseTemplateFileReplace：Kubernetes发布模板文件内容替换（CD）
    HelmDeploy：Kubernetes - helm部署应用
    helm-ci.jenkinsfile：GitOps-基于Helm的CI（代码下载、构建、单元测试、代码扫描、制品上传、镜像构建与上传、Helm发布模板文件内容替换）
    helm-cd.jenkinsfile：GitOps-基于Helm的CD（Helm发布模板文件下载、根据参数替换Helm发布模板文件内容、Helm部署应用、应用健康检查、应用版本回滚）

--- src/org/devops/Kubernetes.groovy
+++ src/org/devops/Kubernetes.groovy
@@ -58,8 +58,76 @@ def K8sReleaseTemplateFileReplace(fileName, fileData, domainName, port, serviceN
 def KubernetesDeploy(namespace, deployFileName, serviceName) {
     // 发布应用至Kubernetes
     sh """
-        kubectl -n ${namespace} apply -f ${deployFileName}
+        kubectl apply -f ${deployFileName}
     """
+
+    // 获取应用状态
+    5.times {
+        sh "sleep 2; kubectl -n ${namespace} get pod | grep ${serviceName}"
+    }
+}
+
+/**
+ * Kubernetes-Helm发布模板文件内容替换并转换Base64（CI）
+ * @param fileName 模板文件名称
+ * @param fileData 模板文件源内容
+ * @param imagePath 镜像仓库地址/镜像名称:镜像标签
+ * 注意：writeYaml data: yamlData 变量值不可以加"${yamlData}"，否则会丢失换行符且文本开头及末尾增加单引号：'
+ */
+def HelmReleaseTemplateFileReplaceAndConvertToBase64(fileName, fileData, imageName, imageTag) {
+    // 删除本地旧文件
+    sh "rm -fr ${fileName}"
+    // 替换模板文件内容
+    yamlData = readYaml text: "${fileData}"
+    yamlData.image.repository = "${imageName}"
+    yamlData.image.tag = "${imageTag}"
+    // 根据文件内容生成新文件
+    writeYaml charset: "UTF-8", overwrite: "true", file: "${fileName}", data: yamlData
+    sh "cat ${fileName}"
+
+    // 获取标准输出返回内容（获取替换后的模板文件内容，封装到Groovy变量中）
+    newYaml = sh returnStdout: true, script: "cat ${fileName}"
+    // 转换文件内容编码格式：Base64
+    return "${newYaml}".bytes.encodeBase64().toString()
+}
+
+/**
+ * Kubernetes发布模板文件内容替换（CD）
+ * @param filePath Git模板文件路径：项目服务名称_Helm/values.yaml
+ * @param domainName 访问域名
+ * @param port 端口号
+ * 注意：writeYaml data: yamlData 变量值不可以加"${yamlData}"，否则会丢失换行符且文本开头及末尾增加单引号：'
+ */
+def HelmReleaseTemplateFileReplace(filePath, domainName, port) {
+    // 替换模板文件内容
+    yamlData = readYaml file: "${filePath}"
+    yamlData.ingress.hosts[0].host = "${domainName}"
+    yamlData.service.port = "${port}"
+    // 根据文件内容生成新文件
+    writeYaml charset: "UTF-8", overwrite: "true", file: "${filePath}", data: yamlData
+    sh "cat ${filePath}"
+}
+
+/**
+ * Kubernetes - Helm部署应用
+ * @param namespace 命名空间
+ * @param deployFilePath 部署文件路径（Helm Chart）
+ * @param serviceName 服务/项目名称
+ */
+def HelmDeploy(namespace, deployFilePath, serviceName) {
+    // Helm发布应用至Kubernetes
+    sh """
+        pwd
+        helm package "${deployFilePath}"
+        helm upgrade --install --create-namespace "${serviceName}" "${serviceName}"-*.tgz -n ${namespace}
+        helm history "${serviceName}" -n ${namespace}
+    """
+
+    //获取release的历史版本
+    env.revision = sh returnStdout: true, script: """ helm history ${serviceName} -n ${namespace} | grep -v 'REVISION' | awk '{print \$1}' """
+    println("${env.revision}".split('\n').toString())
+    env.REVISION = "${env.revision}".split('\n').toString()
+    println("${env.REVISION}")
 
     // 获取应用状态
     5.times {
