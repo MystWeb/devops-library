2941f4218b326595a0813f392b4799f5564e25e4
ziming.xing <77784423@qq.com>
2023-12-05 17:56:38 +0800

    feature(helm-ci): - Checkout.groovy：GitSCM适配git submodule update --init --recursive，递归检出子模块项目源码内容
    - Checkout：GitSCM适配git submodule update --init --recursive，递归检出子模块项目源码内容

--- src/org/devops/Checkout.groovy
+++ src/org/devops/Checkout.groovy
@@ -11,7 +11,17 @@ def GetCode(srcUrl, branchName, credentialsId) {
     checkout([
             $class: 'GitSCM',
             branches: [[name: branchName]],
-            extensions: [],
+            extensions: [
+                    [
+                            $class: 'SubmoduleOption',
+                            disableSubmodules: false,
+                            parentCredentials: false,
+                            recursiveSubmodules: true, // 如果需要递归检出子模块的源码内容，请设置为 true
+                            reference: '', // 可选，可以使用的提交哈希或标签进行子模块检出
+                            trackingSubmodules: false // 如果需要跟踪子模块的提交，请设置为 true
+                    ]
+            ],
+            submoduleCfg: [],
             userRemoteConfigs: [[credentialsId: credentialsId, url: srcUrl]]
     ])
 }
